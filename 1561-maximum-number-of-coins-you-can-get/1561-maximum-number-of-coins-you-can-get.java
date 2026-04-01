8d1b8014d8f6e2381c4a91b9ff154ec8bf5d5d04
Nitin <nitinydv898989@gmail.com>
2026-04-01 18:45:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -1,16 +1,13 @@
 class Solution {
     public int maxCoins(int[] piles) {
-        int maxcoins = 0;
         Arrays.sort(piles);
-        int i = 0,j = piles.length-1;
+        int n = piles.length;
+        int coins = 0;
 
-        while(i < j){
-            j--;
-            maxcoins += piles[j];
-            j--;
-            i++;
+        for (int i = n / 3; i < n; i += 2) {
+            coins += piles[i];
         }
-        return maxcoins;
-        
+
+        return coins;
     }
 }

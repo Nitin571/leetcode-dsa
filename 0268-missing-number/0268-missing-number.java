136c4bbd64c1d9c38320147c91dde967265c8ba3
Nitin <nitinydv898989@gmail.com>
2026-04-06 01:46:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-
-        int expected = n * (n + 1) / 2;
-        int actual = 0;
-
-        for(int i = 0;i<nums.length;i++){
-            actual += nums[i];
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(i != nums[i]){
+                return i;
+            }
         }
-
-        return expected-actual;
+        return nums.length;
     }
 }

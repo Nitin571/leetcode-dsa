9348659d77278c125c7a9cce823322fa33f819ec
Nitin <nitinydv898989@gmail.com>
2026-04-05 15:37:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,6 +1,6 @@
 class Solution {
-    ArrayList<String> al = new ArrayList<>();
     public List<String> fizzBuzz(int n) {
+        ArrayList<String> al = new ArrayList<>();
         for(int i=1;i<=n;i++){
             if(i%3 == 0 && i%5 == 0){
             al.add("FizzBuzz");

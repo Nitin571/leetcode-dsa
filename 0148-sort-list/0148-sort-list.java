71c1e686746d67dbdb49632eaa2237a67e59c7d1
Nitin <nitinydv898989@gmail.com>
2026-04-23 18:50:18 +0530

    Time: 10 ms (52.05%), Space: 59.6 MB (34.34%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode findmid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next!= null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode merge(ListNode head1,ListNode head2){
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+        while(head1 != null && head2 != null){
+            if(head1.val > head2.val){
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            } else{
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            }  
+        }
+
+        if(head1 != null){
+            temp.next = head1;
+        }
+        if(head2 != null){
+            temp.next = head2;
+        }
+
+        return dummy.next;
+
+    }
+    public ListNode sortList(ListNode head) {
+        if( head == null || head.next == null){
+            return head;
+        }
+
+        ListNode mid = findmid(head);
+        ListNode right = mid.next;
+        mid.next = null;
+        ListNode left = sortList(head);
+        right = sortList(right);
+
+        return merge(left,right);
+    }
+}

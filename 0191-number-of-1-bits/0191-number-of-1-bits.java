1f4f46fa6582aa1089a07d0ffde5f4968f8ec3f3
Nitin <nitinydv898989@gmail.com>
2026-04-23 17:32:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,8 +1,12 @@
 class Solution {
     public int hammingWeight(int n) {
-        if(n == 0){
-            return 0;
+        int count = 0;
+        while(n > 0){
+            if(n & 1 == 1){
+                count++;
+            }
+            n = n >> 1;
         }
-        return (n%2) + hammingWeight(n/2);
+        return count;
     }
 }

0ea7f6e1179c525327330f887028fc0d8597c511
Nitin <nitinydv898989@gmail.com>
2026-04-27 18:02:54 +0530

    Time: 2 ms (91.45%), Space: 46.3 MB (45.96%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> s = new Stack<>();
+        int i = 0;
+        int j = 0;
+        while(i < pushed.length){
+            s.push(pushed[i]);
+            i++;
+            while(!s.isEmpty() && j < popped.length && s.peek() == popped[j]){
+                s.pop();
+                j++;
+            }
+        }
+        return s.isEmpty();
+    }
+}

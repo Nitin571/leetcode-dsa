27ba93fb05cd9c56effe4380878cef4d6d493c6c
Nitin <nitinydv898989@gmail.com>
2026-05-09 14:44:43 +0530

    Time: 23 ms (55.37%), Space: 48.4 MB (48.63%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,19 +1,27 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        ArrayList<Integer> al = new ArrayList<>();
+        int n = profit.length;
+
+        int jobs[][] = new int[n][2];
+        for(int i=0;i<n;i++){
+            jobs[i][0] = difficulty[i];
+            jobs[i][1] = profit[i];
+        }
+
+        Arrays.sort(jobs , (a,b) -> a[0]-b[0]);
+        Arrays.sort(worker);
+
+        int total = 0;
+        int maxprofit = 0;
+        int j=0;
+        
         for(int i=0;i<worker.length;i++){
-            int max=0;
-            for(int j=0;j<difficulty.length;j++){
-                if(worker[i] >= difficulty[j]){
-                    max = Math.max(max,profit[j]);
-                }
+            while(j<n && jobs[j][0] <= worker[i]){
+                maxprofit = Math.max(maxprofit,jobs[j][1]);
+                j++;
             }
-            al.add(max);
+            total += maxprofit;
         }
-        int s=0;
-        for(int i=0;i<al.size();i++){
-            s += al.get(i);
-        }
-        return s;
+        return total;
     }
 }

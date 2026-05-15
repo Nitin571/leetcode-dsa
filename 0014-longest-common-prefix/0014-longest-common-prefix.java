8cf89cc94af64fb4c9cdbd69d5e1e0dc3c41175b
Nitin <nitinydv898989@gmail.com>
2026-05-15 17:57:30 +0530

    Time: 1 ms (62.9%), Space: 43.3 MB (49.87%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,71 +1,18 @@
 class Solution {
-    static class Node{
-        Node[] children = new Node[26];
-        boolean eow = false;
-
-        Node(){
-            for(int i = 0; i < 26; i++){
-                children[i] = null;
-            }
-        }
-    }
-    public static Node root = new Node();
-
-    public static void insert(String word){
-
-        Node curr = root;
-
-        for(int i = 0; i < word.length(); i++){
-
-            int idx = word.charAt(i) - 'a';
-
-            if(curr.children[idx] == null){
-                curr.children[idx] = new Node();
-            }
-
-            curr = curr.children[idx];
-        }
-
-        curr.eow = true;
-    }
-    public static int countChildren(Node node){
-
-        int count = 0;
-
-        for(int i = 0; i < 26; i++){
-
-            if(node.children[i] != null){
-                count++;
-            }
-        }
-
-        return count;
-    }
-
     public String longestCommonPrefix(String[] strs) {
-        root = new Node();
-        
-        for(int i = 0; i < strs.length; i++){
-            insert(strs[i]);
-        }
+        StringBuilder result= new StringBuilder();
 
-        String ans = "";
+        Arrays.sort(strs);
 
-        Node curr = root;
+        char[] first= strs[0].toCharArray();
+        char[] last= strs[strs.length-1].toCharArray();
 
-        while(countChildren(curr) == 1 && curr.eow == false){
-            for(int i = 0; i < 26; i++){
-
-                if(curr.children[i] != null){
-
-                    ans += (char)(i + 'a');
-
-                    curr = curr.children[i];
-
-                    break;
-                }
+        for(int i=0; i<first.length; i++){
+            if(first[i] != last[i])
+                break;
+            result.append(first[i]);
             }
+
+            return result.toString();
         }
-        return ans;
     }
-}

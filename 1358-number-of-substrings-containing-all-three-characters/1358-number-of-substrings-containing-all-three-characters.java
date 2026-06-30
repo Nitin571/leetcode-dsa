ed91410c1d434087dc95a5153965a5aaf290366f
Nitin <nitinydv898989@gmail.com>
2026-06-30 11:45:57 +0530

    Time: 14 ms (62.11%), Space: 46.1 MB (78.62%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -3,12 +3,13 @@ class Solution {
     public int numberOfSubstrings(String s) {
         int left = 0;
         int ans = 0;
+        int n = s.length();
         int[] freq = new int[3];
 
-        for (int right = 0; right < s.length(); right++) {
+        for (int right = 0; right < n; right++) {
             freq[s.charAt(right) - 'a']++;
             while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
-                ans += s.length() - right;
+                ans += n - right;
                 freq[s.charAt(left) - 'a']--;
                 left++;
             }

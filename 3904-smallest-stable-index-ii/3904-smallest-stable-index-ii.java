6451ad7ee1830181e2ff37c7812e7478037363b0
Nitin <nitinydv898989@gmail.com>
2026-09-05 22:57:56 +0530

    Time: 6 ms (45.8%), Space: 126.2 MB (95.04%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,31 +1,31 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
-        int prefixMax[] = new int[n];
-        int suffixMin[] = new int[n];
+        int Max[] = new int[n];
+        int Min[] = new int[n];
 
-        prefixMax[0] = nums[0];
+        Max[0] = nums[0];
 
         for(int i=1;i<n;i++){
-            if(nums[i] > prefixMax[i-1]){
-                prefixMax[i] = nums[i];
+            if(nums[i] > Max[i-1]){
+                Max[i] = nums[i];
             }else{
-                prefixMax[i] = prefixMax[i-1];
+                Max[i] = Max[i-1];
             }
         }
 
-        suffixMin[n-1] = nums[n-1];
+        Min[n-1] = nums[n-1];
         for(int i=n-2;i>=0;i--){
-            if(nums[i] < suffixMin[i+1]){
-                suffixMin[i] = nums[i];
+            if(nums[i] < Min[i+1]){
+                Min[i] = nums[i];
             }
             else{
-                suffixMin[i] = suffixMin[i+1];
+                Min[i] = Min[i+1];
             }
         }
 
-        for(int i=0;i<nums.length;i++){
-            int a = prefixMax[i] - suffixMin[i];
+        for(int i=0;i<n;i++){
+            int a = Max[i] - Min[i];
             if(a <= k){
                 return i;
             }

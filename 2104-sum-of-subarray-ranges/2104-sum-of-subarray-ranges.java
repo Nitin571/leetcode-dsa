8143e04023138c066524415b3d2790ea9de77681
Nitin <nitinydv898989@gmail.com>
2026-09-25 15:47:42 +0530

    Time: 23 ms (53.48%), Space: 47.7 MB (8.75%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,16 +1,60 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        int n=nums.length;
-        long sum=0;
+        int n = nums.length;
+        int[] prevSmall = new int[n];
+        int[] nextSmall = new int[n];
+        int[] prevLarge = new int[n];
+        int[] nextLarge = new int[n];
+
+        // previous smaller
+        Stack<Integer> st = new Stack<>();
         for(int i=0;i<n;i++){
-            int min=nums[i];
-            int max=nums[i];
-            for(int j=i;j<n;j++){
-                min=Math.min(min,nums[j]);
-                max=Math.max(max,nums[j]);
-                sum+=(max-min);
+            while(!st.isEmpty() && nums[i] <= nums[st.peek()]){
+                st.pop();
             }
+            prevSmall[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
         }
-        return sum;
+
+        // next smaller 
+        st.clear();
+        for (int i = n-1;i >= 0;i--) {
+            while (!st.isEmpty() && nums[st.peek()] > nums[i]) {
+                st.pop();
+            }
+            nextSmall[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        // previous greater
+        st.clear();
+        for (int i=0;i<n;i++) {
+            while (!st.isEmpty() && nums[st.peek()] <= nums[i]) {
+                st.pop();
+            }
+            prevLarge[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        // next greater
+        st.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] < nums[i]) {
+                st.pop();
+            }
+            nextLarge[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+        long minSum = 0;
+        long maxSum = 0;
+        for (int i = 0; i < n; i++) {
+            long left = i - prevSmall[i];
+            long right = nextSmall[i] - i;
+            minSum += (long) nums[i] * left * right;
+            left = i - prevLarge[i];
+            right = nextLarge[i] - i;
+            maxSum += (long) nums[i] * left * right;
+        }
+        return maxSum - minSum;
     }
 }

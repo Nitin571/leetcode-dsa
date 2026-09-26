01db206321118e24bb2b652c17db4fdc5c2ed0aa
Nitin <nitinydv898989@gmail.com>
2026-09-26 11:26:21 +0530

    Time: 39 ms (46.15%), Space: 93.9 MB (49.77%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -1,35 +1,28 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
-
-        HashMap<String, String> map = new HashMap<>();
-
-        for (List<String> pair : knowledge) {
-            map.put(pair.get(0), pair.get(1));
+        HashMap<String,String> map = new HashMap<>();
+        for (int i=0; i<knowledge.size(); i++) {
+            map.put(knowledge.get(i).get(0), knowledge.get(i).get(1));
         }
-
         StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-
-            if (s.charAt(i) == '(') {
-
-                int j = i + 1;
-
-                while (s.charAt(j) != ')') {
-                    j++;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '('){
+                i++;
+                StringBuilder sub = new StringBuilder();
+                while(s.charAt(i) != ')'){
+                    sub.append(s.charAt(i));
+                    i++;
                 }
-
-                String key = s.substring(i + 1, j);
-
-                sb.append(map.getOrDefault(key, "?"));
-
-                i = j;
-
-            } else {
+                String a = sub.toString();
+                if(map.containsKey(a)){
+                    sb.append(map.get(a));
+                }else{
+                    sb.append('?');
+                }
+            }else{
                 sb.append(s.charAt(i));
             }
         }
-
         return sb.toString();
     }
 }
